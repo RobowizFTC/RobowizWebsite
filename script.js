65ecc81e6ccd4471433f47bda52f92ef64596c39
2017ragrawal <radhikabarbie@gmail.com>
2015-08-14 21:45:35 -0400

    Added scroll effect, generally cleaned up some code, created slider in Our Story section, and created text box popup on click for Our Team section

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
-$(window).load(function(){$(function() {
-    // Stick the #nav to the top of the window
-    var nav = $('#nav');
-    var navHomeY = top;
-    var isFixed = false;
-    var $w = $(window);
-    $w.scroll(function() {
-        var scrollTop = $w.scrollTop();
-        var shouldBeFixed = scrollTop > navHomeY;
-        if (shouldBeFixed && !isFixed) {
-            nav.css({
-                position: 'fixed',
-                top: 0,
-                left: nav.offset().left,
-                width: nav.width()
-            });
-            isFixed = true;
-        }
-        else if (!shouldBeFixed && isFixed)
-        {
-            nav.css({
-                position: 'static'
-            });
-            isFixed = false;
+// $(window).load(function(){$(function() {
+//     // Stick the #nav to the top of the window
+//     var nav = $('#nav');
+//     var navHomeY = top;
+//     var isFixed = false;
+//     var $w = $(window);
+//     $w.scroll(function() {
+//         var scrollTop = $w.scrollTop();
+//         var shouldBeFixed = scrollTop > navHomeY;
+//         if (shouldBeFixed && !isFixed) {
+//             nav.css({
+//                 position: 'fixed',
+//                 top: 0,
+//                 left: nav.offset().left,
+//                 width: nav.width()
+//             });
+//             isFixed = true;
+//         }
+//         else if (!shouldBeFixed && isFixed)
+//         {
+//             nav.css({
+//                 position: 'static'
+//             });
+//             isFixed = false;
+//         }
+//     });
+// });
+// });  
+
+
+function scrollToId(id, speed, callback) {
+    scrollToNumber($(id).offset().top, speed, callback);
+}
+
+function scrollToNumber(number, speed, callback) {
+    $("html, body").animate({
+        scrollTop: number
+    }, speed, function() {
+        if(callback !== undefined) {
+            callback();
         }
     });
-});
-});  
+}
 
 $('body').height($(window).height());
-        }
-        $(document).ready(function() {
-            $(window).bind('resize', set_thing_height);
-            set_height();
-        });
+        
+$(document).ready(function() {
+    var scrollingSpeed = 600;
 
+    // $(window).bind('resize', set_thing_height);
+    // set_height();
 
+    // SiteHeaderNav Links
+    $("#nav a").click(function(e) {
+        e.preventDefault();
+        scrollToId($(this).attr("href"), scrollingSpeed);
+    });
 
+    $("#main-carousel").slick();
 
+    $(".members > li > div").click(function() {
+        var that = this;
+        $(".more-info > div").slideUp(200);
+        setTimeout(function() {
+            $(".more-info > div[data-name=" + $(that).attr("id") + "]").slideDown(200);
+        }, 200);
+    });
 
-
-
-
+    $(".more-info .x").click(function() {
+        $(".more-info > div").slideUp(200);
+    });
+});
